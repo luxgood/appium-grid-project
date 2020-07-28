33adbb1567eac08676036eff9955a21632385a98
luxgood <r.omitsuki@gmail.com>
2020-07-28 23:02:57 +0200

    fixed test

--- src/test/java/AppiumTest2.java
+++ src/test/java/AppiumTest2.java
@@ -4,7 +4,12 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -13,27 +18,38 @@ import java.util.concurrent.TimeUnit;
 
 public class AppiumTest2 {
 
+  AppiumDriver driver;
 
-    @Test
-    public void test() throws MalformedURLException {
-        //setup the web driver and launch the webview app.
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
-        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-        URL url = new URL("http://192.168.8.123:4723/wd/hub");
-        AppiumDriver driver = new AppiumDriver(url, desiredCapabilities);
+  @FindBy(css = "#catapultCookie")
+  WebElement coockieConfirmBtn;
 
-// Navigate to the page and interact with the elements on the guinea-pig page using id.
-        driver.get("https://ageofsigmar.com/");
-        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
-        WebElement factionsBtn = driver.findElement(By.cssSelector("body > div.page-wrap.background-texture > section > section.gtm-subnav.additional-options > div > a:nth-child(2) > p"));
-        factionsBtn.click();
-        WebElement factionsH1 = driver.findElement(By.cssSelector("#aos-page-contents > section.page-hero.page-hero--factions > div > h1"));
-        Assert.assertEquals("Factions", factionsH1.getText()); //check the text retrieved matches expected value
+  @FindBy(xpath = "//p[@class='additional-options__item-text' and . = 'Factions']")
+  WebElement factionsBtn;
 
-//close the app.
-        driver.quit();
+  @BeforeTest
+  public void beforeTest() throws MalformedURLException {
+    // setup the web driver and launch the webview app.
+    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    desiredCapabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+    desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+    desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+    URL url = new URL("http://192.168.8.123:4723/wd/hub");
+    driver = new AppiumDriver(url, desiredCapabilities);
 
-    }
+    PageFactory.initElements(driver, this);
+  }
+
+  @Test
+  public void test() {
+    driver.get("https://ageofsigmar.com/");
+    Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+    coockieConfirmBtn.click();
+    factionsBtn.click();
+  Uninterruptibles.sleepUninterruptibly(5,TimeUnit.SECONDS);
+  }
+
+  @AfterTest
+  public void afterTest() {
+    driver.quit();
+  }
 }
